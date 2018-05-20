d9b0a8bbf5f5ad717479f94e3f9da07f251e118d
Nicholas Lewanowicz <nicklewanowicz@gmail.com>
2018-05-19 21:53:59 -0400

    Add: Initial project model

---
+++ models/project.js
@@ -0,0 +1,13 @@
+
+
+const mongoose = require('mongoose');
+
+const projectSchema = new mongoose.Schema({
+  title: {
+    type: String,
+    unique: false,
+    required: true,
+  },
+});
+
+module.exports = mongoose.model('Project', projectSchema);
