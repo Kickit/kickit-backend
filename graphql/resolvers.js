156a486c80a74f4c038a9656469a01574bcb7d56
Nicholas Lewanowicz <nicklewanowicz@gmail.com>
2018-05-23 18:01:56 -0400

    Add: Resolver for related projects of a user

--- graphql/resolvers.js
+++ graphql/resolvers.js
@@ -1,25 +1,49 @@
 const resolvers = (models) => ({
+  User: {
+    async projects(user) {
+      console.log(user)
+      return await models.Project.find(
+        {
+           _id: { $in: user.projects }
+        }
+     )
+    },
+  },
+
   Query: {
     getUserById(root, { id }) {
       return models.User.findById(id).then((response) => response);
     },
 
-    getUserByEmail(root, { email }) {
-      return models.User.findOne({ email }).then((response) => response);
+    async getUserByEmail(root, { email }) {
+      return await models.User.findOne({ email })
     },
+
+    async getProjById(root, { projectId }) {
+      return await models.Project.findById(projectId)
+    }
   },
   Mutation: {
     createUser(root, args) {
       const user = new models.User(args);
       return user.save().then((response) => response);
     },
-    createProj(root, args) {
+    async createProj(root, args) {
       console.log(root,args)
       const proj = new models.Project(args);
+      proj.owners = [args.owner]
+      let owner = await models.User.findById(args.owner)
+      owner.projects.push(proj._id)
+      console.log('need to update', owner)
+      await models.User.update({ _id: args.owner} , { $set: {projects: owner.projects} })
       proj.created = Math.floor(new Date() / 1000)
+      proj.owners = [args.owner]
+      console.log(proj)
+      console.log(owner)
       return proj.save().then((response) => response);
     },
     createSect(root, args) {
+      debugger
       const sect = new models.Section(args);
       return sect.save().then((response) => response);
     },
