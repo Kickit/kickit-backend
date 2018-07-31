ec18a4d789f8822ed66a27394081c482a8d3ce92
Nicholas Lewanowicz <nicklewanowicz@gmail.com>
2018-07-30 22:34:07 -0400

    Add: tabs array to default graphiql setup

---
+++ tabs.js
@@ -0,0 +1,10 @@
+// This is where the tabs for the graphiql interface are defined. 
+// Feel free to add more tabs for more default mutations and queries.
+
+module.exports = [{
+  query: '# Welcome to the Kickit GraphiQL interface!',
+  headers: {
+    "Authorization": "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOiI1YjFjMzg1NGQ3Yzk4ZDNkMzJmOTYxZmQiLCJpYXQiOjE1Mjg1NzYwODR9.CVF0DRe24HT7ZLzG7K6v_6WYUcGx5wu-lOWIietGb5k"
+  },
+  endpoint: '/graphql'
+}]
